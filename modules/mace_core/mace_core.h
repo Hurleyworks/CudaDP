9d433ee9d97aa5c6cc685d1db54f3bbde2f8127b
Steve Hurley <github@hurleyworks.com>
2022-05-22 11:50:41 -0400

    reduce and clean

--- modules/mace_core/mace_core.h
+++ modules/mace_core/mace_core.h
@@ -1,32 +1,19 @@
 #pragma once
 
-#include <unordered_map>
-#include <unordered_set>
-#include <array>
-#include <queue>
-#include <stack>
-#include <fstream>
-#include <set>
-#include <vector>
-#include <sstream>
-#include <iomanip> // byteStr()
-#include <random>
-#include <chrono>
-#include <thread>
-#include <ctime>
-#include <string>
 #include <iostream>
-#include <stdexcept>
-#include <assert.h>
-#include <limits>
-#include <algorithm>
-#include <functional>
-#include <stdint.h>
-#include <any>
-#include <filesystem>
-#include <mutex>
-#include <memory>
-#include <condition_variable>
+
+#define WIN32_LEAN_AND_MEAN
+#if defined(_WIN32) || defined(_WIN64)
+#define __FUNCTION_NAME__ __func__
+#define _FN_ __FUNCTION_NAME__
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <Windows.h>
+#undef near
+#undef far
+#undef RGB
+#endif
 
 // NO MORE TRACING  Yay
 #define TRACE(x)
@@ -35,6 +22,12 @@
 #include <g3log/g3log.hpp>
 #include <g3log/logworker.hpp>
 
+const LEVELS TESTING{INFO.value + 1, "TESTING"};
+const LEVELS CRITICAL{WARNING.value + 1, "CRTICAL"};
+
+struct DesktopWindowSettings{};
+
+#if 0
 // some useful tools and defines outside mace namespace
 #include "excludeFromBuild/basics/Defaults.h"
 #include "excludeFromBuild/basics/Util.h"
@@ -44,3 +37,5 @@ namespace mace
 #include "excludeFromBuild/basics/StringUtil.h"
 
 } // namespace mace
+
+#endif
